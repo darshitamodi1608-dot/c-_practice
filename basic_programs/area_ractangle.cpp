6c5d973274b7750649f88728f49e5639bd01c676
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-21 10:42:26 +0530

    add basic programs

---
+++ basic_programs/area_ractangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float breadth,length,area;
+	cout<<"Enter lenght:";
+	cin>>length;
+	cout<<"Enter breadth:";
+	cin>>breadth;
+	area=breadth*length;
+	cout<<"Area of ractangle is "<<area;
+	return 0;
+}
