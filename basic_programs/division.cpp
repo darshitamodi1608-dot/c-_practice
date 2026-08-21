8dbbdb08dda65b57b2e51046d1ad5b759436a536
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-21 10:40:13 +0530

    add basic programs

---
+++ basic_programs/division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2,div;
+	cout<<"Enter n1 :";
+	cin>>n1;
+	cout<<"Enter n2 : ";
+	cin>>n2;
+	div=n1/n2;
+	cout<<"Division is "<<div;
+	return 0;
+}
