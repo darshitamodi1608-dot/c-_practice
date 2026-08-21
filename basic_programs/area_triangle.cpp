92a4068341ac7f861e670e90e1f416a5ba316c0b
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-21 10:43:13 +0530

    add basic programs

---
+++ basic_programs/area_triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float base,height,area;
+	cout<<"Enter height:";
+	cin>>height;
+	cout<<"Enter base:";
+	cin>>base;
+	area=0.5*base*height;
+	cout<<"Area of triangle is "<<area;
+	return 0;
+}
