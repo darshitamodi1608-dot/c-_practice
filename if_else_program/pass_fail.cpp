ccebc715e129d071231f229491d975edec8d4536
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-23 14:39:49 +0530

    added to if_else_program

---
+++ if_else_program/pass_fail.cpp
@@ -0,0 +1,14 @@
+/* check student is pass or fail */
+#include<iostream>
+using namespace std;
+int main()
+{
+	int marks;
+	cout<<"Enter marks of student:";
+	cin>>marks;
+	if(marks>=35)	
+	 cout<<"PASS";
+	else 
+     cout<<"FAIL";
+	return 0; 
+}
