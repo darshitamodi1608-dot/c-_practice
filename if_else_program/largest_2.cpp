9dcd8e6ee7c887e6c907cccc54c1149d69262f77
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-23 14:47:06 +0530

    added to if_else_program

---
+++ if_else_program/largest_2.cpp
@@ -0,0 +1,17 @@
+/* check which number is largest in 2 number */
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	cout<<"Enter number 1:";
+	cin>>n1;
+	cout<<"Enter number 2 :";
+	cin>>n2;
+	if(n1>n2)
+	 cout<<"N1 is largest";
+	else 
+	 cout<<"N2 is largest";
+	 
+	return 0; 
+}
