08cc8eca56470bfabd325db089fcf6c088b0e018
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-25 11:33:48 +0530

    added to operators

---
+++ operators/Relational_operators.cpp
@@ -0,0 +1,18 @@
+/* Relational operators */
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter number1:";
+    cin>>a;
+    cout<<"Enter number2:";
+    cin>>b;
+    cout<< "\n a > b  = " << (a > b);
+    cout<< "\n a < b  = " << (a < b);
+    cout<< "\n a >= b = " << (a >= b);
+    cout<< "\n a <= b = " << (a <= b);
+    cout<< "\n a == b = " << (a == b);
+    cout<< "\n a != b = " << (a != b);
+	return 0;	
+}
