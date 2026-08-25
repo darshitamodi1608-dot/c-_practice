fa95f286a61d451e776c2023166c4e4a19b66bc0
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-25 11:33:01 +0530

    added to operators

---
+++ operators/Assignment_operators.cpp
@@ -0,0 +1,16 @@
+/* Assignment Operators */
+#include <iostream>
+using namespace std;
+int main()
+{ int a = 10;
+  cout<<"\n a = "<< a;
+  a += 5; 
+  cout<<"\n a += 5 = "<<a;
+  a -= 3;
+  cout<<"\n a -= 3 = "<<a;
+  a *= 2;
+  cout<<"\n a *= 2 = "<< a;
+  a /= 4;
+  cout<<"\n a /= 4 = "<<a;
+  return 0;
+}
