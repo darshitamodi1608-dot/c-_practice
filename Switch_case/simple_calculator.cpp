6a904eec5f20df2b15aba92bb8dab18521bb7bcb
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-26 12:51:47 +0530

    added to switch case

---
+++ Switch_case/simple_calculator.cpp
@@ -0,0 +1,46 @@
+/* Simple Calculator using Switch Case */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter operator (+, -, *, /, %): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    switch(op)
+    {
+        case '+':
+            cout << "Addition = " << a + b;
+            break;
+
+        case '-':
+            cout << "Subtraction = " << a - b;
+            break;
+
+        case '*':
+            cout << "Multiplication = " << a * b;
+            break;
+
+        case '/':
+            cout << "Division = " << a / b;
+            break;
+
+        case '%':
+            cout << "Remainder = " << a % b;
+            break;
+
+        default:
+            cout << "Invalid Operator";
+    }
+
+    return 0;
+}
