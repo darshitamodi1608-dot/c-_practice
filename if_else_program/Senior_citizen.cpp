058f3c6fb95ca65f03387285872057382235650e
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-26 11:04:05 +0530

    added to if else program

---
+++ if_else_program/Senior_citizen.cpp
@@ -0,0 +1,14 @@
+/*Senior Citizen checker*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter age: ";
+	cin>>age;
+	if(age>=60)
+	cout<<" You are a senior citizen.";
+	else
+	cout<<" You are not a senior citizen.";
+	return 0;
+}
