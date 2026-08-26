06e19194b5c55a31e55dedc1090df2ce3aedb46a
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-26 12:52:48 +0530

    added to switch case

---
+++ Switch_case/vowel_consonent.cpp
@@ -0,0 +1,32 @@
+/* Check Vowel or Consonant */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char alpha;
+
+    cout << "Enter any alphabet: ";
+    cin >> alpha;
+
+    switch(alpha)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            cout << "Vowel";
+            break;
+
+        default:
+            cout << "Consonant";
+    }
+
+    return 0;
+}
